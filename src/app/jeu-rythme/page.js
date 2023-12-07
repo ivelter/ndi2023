346554e0a834edaab8e451e057387eaa744a29fc
Bastide Alexis <bastide.alexis@etu.umontpellier.fr>
2023-12-07 18:47:00 +0100

    branche jeu de rythme

---
+++ src/app/jeu-rythme/page.js
@@ -0,0 +1,8 @@
+import * as PIXI from 'pixi.js';
+// Module est important !!
+
+const app = new Application();
+
+export default function PagePourDireBonjour() {
+    return <h1 className={moncss.bonjour}>Bonjour!</h1>
+}
