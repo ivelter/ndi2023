eb88b26f87db3cba6f4a0c3263b2a76259643f22
Bastide Alexis <bastide.alexis@etu.umontpellier.fr>
2023-12-07 19:35:48 +0100

    modif jeu rythme

--- src/app/jeu-rythme/page.js
+++
@@ -1,8 +0,0 @@
-import * as PIXI from 'pixi.js';
-// Module est important !!
-
-const app = new Application();
-
-export default function PagePourDireBonjour() {
-    return <h1 className={moncss.bonjour}>Bonjour!</h1>
-}
