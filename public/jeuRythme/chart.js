31783dbf0c8d7a5dda78ab07effb838854cb4fe8
Bastide Alexis <bastide.alexis@etu.umontpellier.fr>
2023-12-07 20:59:32 +0100

    table test

---
+++ public/jeuRythme/chart.js
@@ -0,0 +1,13 @@
+export default {
+    "notes" : [
+        {"touche" : "l", "delais" : 1000},
+        {"touche" : "u", "delais" : 1500},
+        {"touche" : "r", "delais" : 1900},
+        {"touche" : "d", "delais" : 2400},
+        {"touche" : "d", "delais" : 3000, "durée" : 1000},
+        {"touche" : "r", "delais" : 4000},
+        {"touche" : "l", "delais" : 4200, "durée" : 1200},
+        {"touche" : "u", "delais" : 4800},
+        {"touche" : "u", "delais" : 5000}
+    ]
+}
