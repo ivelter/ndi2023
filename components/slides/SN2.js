2d516ebf7fe3bd1e43ea31eafa3fd0ee4f661bc3
Théo Dubernet <95922914+ErmitePanda@users.noreply.github.com>
2023-12-08 02:21:41 +0100

    tentative de debug

--- components/slides/SN2.js
+++ components/slides/SN2.js
@@ -3,11 +3,14 @@ import Degrade from "../Degrade"
 import FlecheSuiv from "../FlecheSuiv"
 import Image from "next/image"
 import base from "./SN2.module.scss"
+import bg from '../../public/img/slide2-img.png'
 
 export default function SN2() {
     return (
         <Degrade color={all.clair}>
             <div className={base.containerCitation}>
+            <Image src="/img/slide2-img.png" alt="Une illustration simple du soleil, placée dans le coin supérieur gauche de l'écran."
+            width="100" height="100"/>
                 <div className={base.citationSarkozy}>“On a fait une conférence sur le climat. On parle beaucoup de
                     dérèglement climatique, c'est très intéressant mais ça fait 4,5 milliards d'années que le climat change.
                     L'homme n'est pas le seul responsable de ce changement”
