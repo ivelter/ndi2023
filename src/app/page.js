11b5a81c456fc101a4994250eceb299d55b3a168
Thumus <thumus@tutanota.com>
2023-12-08 06:05:25 +0100

    refactor: move everything to src

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import Slides from "../../components/slides/export";
+import Slides from "../components/slides/export";
 
 export default function Home() {
   const [konamiCode, setKonamiCode] = useState([]);
