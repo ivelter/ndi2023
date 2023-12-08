bc4dd83bc674384204d23fe8c8f763f521566960
Thumus <thumus@tutanota.com>
2023-12-08 02:06:30 +0100

    add: metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import './globals.scss'
 const inter_marche = Fira_Code({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Dummy file',
-  description: 'Site généré',
+  title: 'Le réchauffement climatique',
+  description: "Site créé par la `CoopTeam` pendant la Nuit De l'Info édition 2023. Le sujet était sur le réchauffement climatique, déméler le vrai du faux. Nous avons donc créer une expérience unique.",
 }
 
 export default function RootLayout({ children }) {
