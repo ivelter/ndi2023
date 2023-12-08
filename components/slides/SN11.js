a005aeff30570a6005d3e78637956655004e776a
bassiere <eliott.bassier@etu.umontpellier.fr>
2023-12-08 05:12:34 +0100

    refactor : refonte de la page 11

--- components/slides/SN11.js
+++ components/slides/SN11.js
@@ -21,15 +21,21 @@ export default function SN11() {
 
                 <div className={base.d1}>
                     <p className={`${fontLato.className}`}>
-                        jsp écoutez le giec les cop (les conférences pas les flics on dit acab ici) jsp au lieu de sucer trump et zemour
+                        Luttons ensemble contre la désinformation !
+                    </p>
+                    <p className={`${fontLato.className}`}>
+                        Il existe une grande variété de faits concernant le réchauffement climatique étant encore inconnus du grand public, il est essentiel d'être informé sur ces sujets pour un avenir plus sain
+                    </p>
+                    <p className={`${fontLato.className}`}>
+                        Certains sites comme ceux d'organisations spécialisées dans les études climatiques comme le GIEC sont des sources fiables pour récolter des informations sur les enjeux actuels liés au climat.
                     </p>
 
                 </div>
 
                 <div className={base.d2}>
                     <div className={` ${base.dansD2elt1} ${fontQuicksand.className}`}>
-                        <a href="https://macron.fun">
-                        Combattre les fausses informations
+                        <a href="https://www.ecologie.gouv.fr/publication-du-6e-rapport-synthese-du-giec">
+                        Cliquez pour combattre la désinformation
                         </a>
                     
                     </div>
