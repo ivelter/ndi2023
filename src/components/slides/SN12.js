cea3b5b2081325adbb62d3a8d4931c69c70d15bc
bassiere <eliott.bassier@etu.umontpellier.fr>
2023-12-08 06:29:24 +0100

    conflit resolu

--- src/components/slides/SN12.js
+++ src/components/slides/SN12.js
@@ -1,19 +1,21 @@
-import all from "../../variables.module.scss";
-import Degrade from "../Degrade";
-import Image from "next/image";
-import base from "./SN12.module.scss";
+import all from "../../variables.module.scss"
+import Degrade from "../Degrade"
+import Image from "next/image"
+import base from "./SN12.module.scss"
+import FlecheSuiv from "../FlecheSuiv"
 
 export default function SN12() {
-  return (
-    <Degrade color={all.noir} gradientStart={all["gradientM-N"]} id={12}>
-      <div className={base.centerDiv}>
-        <Image
-          src="/img/feu.jpg"
-          alt="Une photo d'un feu placé au centre de l'écran."
-          width="700"
-          height="700"
-        />
-      </div>
-    </Degrade>
-  );
+    return (
+        <Degrade color={all.noir} gradientStart={all["gradientM-N"]}>
+
+            <div className={base.centerDiv}>
+
+                <div className={base.parent}>
+                    <Image src="/img/feu.jpg" alt="Une photo d'un feu placé au centre de l'écran." layout="fill" className={base.image}/>
+                </div>
+
+            </div>
+
+        </Degrade>
+    )
 }
