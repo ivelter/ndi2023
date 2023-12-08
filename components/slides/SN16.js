60973518d8d292030b0ef52ea881062cdaae3036
bassiere <eliott.bassier@etu.umontpellier.fr>
2023-12-08 04:42:18 +0100

    easter egg commentaire

--- components/slides/SN16.js
+++ components/slides/SN16.js
@@ -2,6 +2,11 @@ import all from "../../variables.module.scss"
 import Degrade from "../Degrade"
 import base from "./SN16.module.scss"
 
+const DangerousRawHtml = ({ html = "" }) => (
+    <script dangerouslySetInnerHTML={{ __html: `${html}` }} />
+  );
+
+
 export default function SN16() {
     return (
         <Degrade color="#2E3033">
@@ -20,6 +25,18 @@ export default function SN16() {
 
                 <div className={base.easterEggSlide16}>psst. pensez à cliquer sur le soleil au début.</div>
             </div>
+            <DangerousRawHtml html={`<!--
+            
+            __   __ _______ ______   _______ ___   _______ __   __ __   __ __   __ __   __ _______   __  
+            |  |_|  |       |    _ | |       |   | |       |  | |  |  | |  |  |_|  |  | |  |       | |  | 
+            |       |    ___|   | || |       |   | |_     _|  |_|  |  | |  |       |  | |  |  _____| |  | 
+            |       |   |___|   |_||_|       |   |   |   | |       |  |_|  |       |  |_|  | |_____  |  | 
+            |       |    ___|    __  |      _|   |   |   | |       |       |       |       |_____  | |__| 
+            | ||_|| |   |___|   |  | |     |_|   |   |   | |   _   |       | ||_|| |       |_____| |  __  
+            |_|   |_|_______|___|  |_|_______|___|   |___| |__| |__|_______|_|   |_|_______|_______| |__| 
+            
+            -->`} />
         </Degrade>
+        
     )
 } 
